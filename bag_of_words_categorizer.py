a3638b28832e64d99335a67907d0ef5f6cd1b67b
Yashaswe <amatyayasu@gmail.com>
2024-06-09 22:31:44 +0545

    add definition of bag of word and one hot encoding

--- bag_of_words_categorizer.py
+++ bag_of_words_categorizer.py
@@ -1,3 +1,7 @@
+#Bag of Words model is used to preprocess the text by converting it into a bag of words, which keeps a count of the total occurrences of most frequently used words. It does not capture information about a word's meaning or context.
+#BoW representation provides insights into the importance of different terms within the text. By counting the frequency of words, we can observe which words occur more frequently
+#One hot coding allows words and phrases to be represented as numeric vectors. A unique index is assigned to each unique category (word or phrase), creating a binary vector of length equal to the total number of categories. This vector is called a “one hot” encoded vector because only one element is “hot” or active at a time.
+
 class Category:
   BOOKS = "BOOKS"
   CLOTHING = "CLOTHING"
@@ -7,6 +11,10 @@ train_y = [Category.BOOKS, Category.BOOKS, Category.CLOTHING, Category.CLOTHING]
 
 from sklearn.feature_extraction.text import CountVectorizer
 
+# One Hot Encoding
+# vectorizer = CountVectorizer(binary = True)
+
+# Bag of Words
 vectorizer = CountVectorizer()
 train_x_vectors = vectorizer.fit_transform(train_x)
 
@@ -23,7 +31,7 @@ from sklearn import svm
 clf_svm = svm.SVC(kernel = "linear")
 clf_svm.fit(train_x_vectors, train_y)
 
-test_x = vectorizer.transform(['i like the book'])
+test_x = vectorizer.transform(['i like the book', "i love the story"])
 predicted = clf_svm.predict(test_x)
 print(predicted)
 
